37840e6f967bd51a5e68db505f4bb680abd1382c
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-08 15:38:37 +0500

    lesson 40 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import Header from './components/Header';
 import Meme from './components/Meme';
+import { useState } from 'react';
 import './index.css';
 
 function App() {
+  const [firstname,setfirstname]= useState("");
+  const handlechange=(event)=>{
+    setfirstname(event.target.value)
+    console.log(firstname);
+  }
   return (
-    <div className="App">
-      <Header/>
-      <Meme/>
-    </div>
+    // <div className="App">
+    //   <Header/>
+    //   <Meme/>
+    // </div>
+    <form>
+      <input
+      type="text"
+      placeholder="enter some input"
+      onChange={handlechange} 
+        />
+    </form>
   );
 }
 //Conditional rendering thorouly done
