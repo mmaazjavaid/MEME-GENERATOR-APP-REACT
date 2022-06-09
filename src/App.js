3dd52facc1a4ef64516667360351fb526e1eba91
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-09 13:29:07 +0500

    memes generator form connected

--- src/App.js
+++ src/App.js
@@ -4,74 +4,13 @@ import { useState } from "react";
 import "./index.css";
 
 function App() {
-  const [formdata, setformdata] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    isJoinedNewsletter: false,
-  });
+  return(
 
-  const handlechange = (e) => {
-    const {name,value,checked,type}=e.target;
-    setformdata(prevdata=>{
-      return{
-        ...prevdata,
-        [name]:type==="checkbox"? checked:value
-      }
-    })
-  };
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if(formdata.password===formdata.confirmPassword){
-      console.log("signed up successfully")
-      if(formdata.isJoinedNewsletter===true){
-        console.log("Thanks for signing up to our newsletter")
-      }
-    }else{
-      console.log("passwords does not match")
-    }
-    
-  };
-
-  return (
-    <div className="form-container">
-      <form className="form" onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email address"
-          className="form--input"
-          name="email"
-          onChange={handlechange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="form--input"
-          name="password"
-          onChange={handlechange}
-        />
-        <input
-          type="password"
-          placeholder="Confirm password"
-          className="form--input"
-          name="confirmPassword"
-          onChange={handlechange}
-        />
-
-        <div className="form--marketing">
-          <input 
-          id="okayToEmail"
-          type="checkbox"
-          name="isJoinedNewsletter"
-          checked={formdata.isJoinedNewsletter} 
-          onChange={handlechange} />
-          <label htmlFor="okayToEmail">I want to join the newsletter</label>
-        </div>
-        <button className="form--submit">Sign up</button>
-      </form>
+    <div>
+      <Header/>
+      <Meme/>
     </div>
+
   );
 }
 //Conditional rendering thorouly done
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,30 +1,45 @@
-import React,{useState} from "react"
-import memesData from "../memesData"
-import Button from "./Button"
-import ImageContainer from "./ImageContainer"
+import React from "react"
+import memesData from "../memesData.js"
+import { useState } from "react"
+
 export default function Meme() {
-
-
-    const [meme,setMeme]=useState({
-        topText:" ",
-        bottomText:" ",
-        randomImage:"http://i.imgflip.com/1bij.jpg"
+    /**
+     * Challenge: 
+     * 1. Set up the text inputs to save to
+     *    the `topText` and `bottomText` state variables.
+     * 2. Replace the hard-coded text on the image with
+     *    the text being saved to state.
+     */
+     
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
 
-    const [allMemeImages,setallMemeImages]=useState(memesData)
-
-    const handleMemeImage=()=>{
-        var {url} = allMemeImages.data.memes[Math.floor(Math.random()* allMemeImages.data.memes.length)];
-        setMeme(prevMeme=>{
-            return {
-                ...prevMeme,
-                randomImage:url
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    
+     function getMemeImage() {
+         const memesArray = allMemeImages.data.memes
+         const randomNumber = Math.floor(Math.random() * memesArray.length)
+         const url = memesArray[randomNumber].url
+         setMeme(prevMeme => ({
+             ...prevMeme,
+             randomImage: url
+         }))
+         
+     }
+    
+    const handlechange=(e)=>{
+        const {name,value}=e.target
+        setMeme(prevmeme=>{
+            return{
+                ...prevmeme,
+                [name]:value
             }
         })
-        
     }
-   
-    
     
     return (
         <main>
@@ -33,17 +48,30 @@ export default function Meme() {
                     type="text"
                     placeholder="Top text"
                     className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handlechange}
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handlechange}
                 />
-                <Button on={false} onClick={handleMemeImage}  />
-                
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
             </div>
-            <ImageContainer  randomImage={meme.randomImage} />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
-
